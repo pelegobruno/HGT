8cb7aedc64b0c996d9cdf7e8af1a846778c8948c
pelegobruno <SEU_EMAIL_DO_GITHUB>
2026-06-05 01:41:30 -0300

    fix: remocao da propriedade skipWaiting invalida no next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,14 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
+  register: true, 
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
-  disable: process.env.NODE_ENV === "development",
+  disable: false, 
+  workboxOptions: {
+    disableDevLogs: true,
+  }
 });
 
 const nextConfig: NextConfig = {
